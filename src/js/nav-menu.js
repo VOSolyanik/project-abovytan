852bb6c4acd872568b5dfed062652f394aea3c41
VOSolyanik <v.o.solyanik@gmail.com>
2024-10-03 02:00:17 +0300

    Header markup and styles + mobile menu. Reusable social links

---
+++ src/js/nav-menu.js
@@ -0,0 +1,27 @@
+(() => {
+    const refs = {
+      // Додати атрибут data-modal-open на кнопку відкриття
+      openMenuBtn: document.querySelector('[data-menu-open]'),
+      // Додати атрибут data-modal-close на кнопку закриття
+      closeMenuBtn: document.querySelector('[data-menu-close]'),
+      // Додати атрибут data-modal на бекдроп модалки
+      backdrop: document.querySelector('[data-menu-backdrop]'),
+      menu: document.querySelector('[data-menu]'),
+    };
+
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    refs.backdrop.addEventListener('click', closeMenu);
+
+    window.navigator.addEventListener('navigate', function () {
+      closeMenu();
+    });
+
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-open');
+    }
+
+    function closeMenu() {
+      refs.menu.classList.toggle('is-open');
+    }
+  })();
